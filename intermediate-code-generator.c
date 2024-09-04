1aed7223bd26c0a95f6d967eaf336870f9ecc4fd
Anna Ann Mathew <112563080+AnnaTheSloth284@users.noreply.github.com>
2024-09-04 09:33:09 +0530

    Update intermediate-code-generator.c

--- intermediate-code-generator.c
+++ intermediate-code-generator.c
@@ -5,86 +5,118 @@
 //  Created by Anna Ann Mathew on 03/08/24.
 //
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include <ctype.h>
 
-#define MAX_OP_SIZE 2
-#define MAX_ARG_SIZE 5
-#define MAX_RESULT_SIZE 10  // Increased size for result buffer
+char queue[100];
+char s[100];
+char ind = '1';
+int count2 = 1;
+FILE *fp1,*fp2;
 
-void generate_code(char *op, char *arg1, char *arg2, char *result);
-
-int main() {
-    char input[100];
-    char op[MAX_OP_SIZE], arg1[MAX_ARG_SIZE], arg2[MAX_ARG_SIZE], result[MAX_RESULT_SIZE];
-    char *token;
-
-    printf("Enter the operation (format is either triple or quadruple):\n");
-    fgets(input, sizeof(input), stdin);
-    input[strcspn(input, "\n")] = 0;  // Remove trailing newline
-
-    // Tokenize the input
-    token = strtok(input, " ");
-    
-    // Read operation and operands
-    if (token != NULL) {
-        strcpy(op, token); // Operation (e.g., +, *, -)
-        token = strtok(NULL, " ");
-        
-        if (token != NULL) {
-            strcpy(arg1, token); // First operand
-            token = strtok(NULL, " ");
-            
-            if (token != NULL) {
-                strcpy(arg2, token); // Second operand
-                
-                // Check if there's a fourth token for quadruple format
-                token = strtok(NULL, " ");
-                if (token != NULL) {
-                    strncpy(result, token, MAX_RESULT_SIZE - 1);
-                    result[MAX_RESULT_SIZE - 1] = '\0'; // Ensure null termination
-                } else {
-                    // If there is no fourth token, assume triple format
-                    strncpy(result, "RESULT", MAX_RESULT_SIZE - 1);
-                    result[MAX_RESULT_SIZE - 1] = '\0'; // Ensure null termination
-                }
-                
-                generate_code(op, arg1, arg2, result);
-            } else {
-                fprintf(stderr, "Invalid input format.\n");
-            }
-        } else {
-            fprintf(stderr, "Invalid input format.\n");
-        }
-    } else {
-        fprintf(stderr, "Invalid input format.\n");
+int precedence(char c){
+    if(c == '+' || c == '-'){
+        return 1;
     }
-
-    return EXIT_SUCCESS;
+    else if(c == '*' || c == '/'){
+        return 2;
+    }
+    else if(c == '^'){
+        return 3;
+    }
+else{
+return 0;
+}
 }
 
-void generate_code(char *op, char *arg1, char *arg2, char *result) {
-    if (strcmp(op, "+") == 0) {
-        printf("\nMOV R0, %s", arg1);
-        printf("\nADD R0, %s", arg2);
-        printf("\nMOV %s, R0", result);
-    } else if (strcmp(op, "*") == 0) {
-        printf("\nMOV R0, %s", arg1);
-        printf("\nMUL R0, %s", arg2);
-        printf("\nMOV %s, R0", result);
-    } else if (strcmp(op, "-") == 0) {
-        printf("\nMOV R0, %s", arg1);
-        printf("\nSUB R0, %s", arg2);
-        printf("\nMOV %s, R0", result);
-    } else if (strcmp(op, "/") == 0) {
-        printf("\nMOV R0, %s", arg1);
-        printf("\nDIV R0, %s", arg2);
-        printf("\nMOV %s, R0", result);
-    } else if (strcmp(op, "=") == 0) {
-        printf("\nMOV R0, %s", arg1);
-        printf("\nMOV %s, R0", result);
-    } else {
-        fprintf(stderr, "Unknown operation: %s\n", op);
+void formatter(char post[100],char stack[100]){
+int top = -1, j = 0, front = -1;
+int n = strlen(s), i = 0;
+while(s[i] != '\0'){
+if(s[i] == '('){
+top++;
+stack[top] = s[i];
+}
+else if(isalpha(s[i])){
+post[j++] = s[i];
+}
+else if(precedence(s[i])){
+while(precedence(stack[top]) >= precedence(s[i])){
+post[j++] = stack[top--];
+}
+top++;
+stack[top] = s[i];
+}
+else if(s[i] == ')'){
+while(stack[top] != '('){
+post[j++] = stack[top--];
+}
+top--;
+}
+i++;
+}
+while(top!=-1){
+post[j++] = stack[top--];
+}
+post[j] = '\0';
+printf("Postfix expression: %s\n", post);
+i = 0;
+while(post[i] != '\0'){
+if(precedence(post[i])){
+char a = queue[front--];
+char b = queue[front--];
+
+if(isdigit(a) && isdigit(b)){
+fprintf(fp1,"%c\tt%c\tt%c\tt%c\n", post[i], b, a, ind);
+//fprintf(fp1,"%c\tt%c\tt%c\tt%d\n", post[i], b, a, count2);
+}
+else if(isdigit(b)){
+fprintf(fp1,"%c\tt%c\t%c\tt%c\n", post[i], b, a, ind);
+//fprintf(fp1,"%c\tt%c\t%c\tt%d\n", post[i], b, a, count2);
+}
+else if(isdigit(a)){
+fprintf(fp1,"%c\t%c\tt%c\tt%c\n", post[i], b, a, ind);
+//fprintf(fp1,"%c\t%c\tt%c\tt%d\n", post[i], b, a, count2);
+}
+else{
+fprintf(fp1,"%c\t%c\t%c\tt%c\n", post[i], b, a, ind);
+//fprintf(fp1,"%c\t%c\t%c\tt%d\n", post[i], b, a, count2);
+}
+fprintf(fp2,"%c\t%c\t%c\t%c\n", ind, post[i], b, a);
+//fprintf(fp2,"%d\t%c\t%c\t%c\n", count2, post[i], b, a);
+front++;
+queue[front] = ind;
+ind++;
+count2++;
+}
+else{
+front++;
+queue[front] = post[i];
+}
+i++;
+}
+}
+
+void main(){
+    int count;
+    fp1 = fopen( "quadruple-format.txt","w");
+    fp2 = fopen( "triple-format.txt","w");
+    fprintf(fp1,"QUADRUPLE\nOP\tO1\tO2\tRES\n");
+    fprintf(fp2,"TRIPLE\nIN\tOP\tO1\tO2\n");
+
+    printf("Enter the number of expressions:");
+    scanf("%d",&count);
+
+    int k;
+    for(k=0;k<count;k++){
+        memset(queue, '\0', sizeof(queue));
+        memset(s, '\0', sizeof(s));
+        char post[100], stack[100];
+        printf("Infix expression: ");
+        scanf("%s", s);
+        formatter(post, stack);
     }
+
 }
